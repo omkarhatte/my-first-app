cba141c881c99d14c57e42e606ec9bd5501910b6
omkarhatte <om.hatte2@gmail.com>
2022-04-25 18:16:28 +0530

    "add two integers"

---
+++ banking-app/src/main/java/com/dlithe/bankingapp/practice/Demo.java
@@ -0,0 +1,21 @@
+package com.dlithe.bankingapp.practice;
+
+/*This class is copyright property of Dlithe organization it is open to all and intended to use for with care*/
+public class Demo {
+
+    /*
+    *This class is used to give demo on how to commit and push your local changes to your feature branch
+    * @author om
+    * @version 1.0
+    * @since 1.0
+    * */
+
+
+    public void addNumbers(int a, int b){
+        a=2;
+        b=10;
+        System.out.println(a+b);
+    }
+
+
+}
